ecab5c49eb5f5c1c63852ea040f0472965905b06
Barabanshchikova Anastasya <nastya27788@gmail.com>
2018-10-31 13:19:08 +0500

    1 вариант решения, пока без допа

--- emitter.js
+++ emitter.js
@@ -4,13 +4,23 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const events = {};
+
+    function subscribe({ event, context, handler, times, frequency }) {
+        if (!events[event]) {
+            events[event] = [];
+        }
+
+        events[event].push({ context, handler, times, frequency, count: 0 });
+    }
+
     return {
 
         /**
@@ -18,26 +28,64 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {any}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            subscribe({ event, context, handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {any}
          */
         off: function (event, context) {
             console.info(event, context);
+            events[event] = events[event].filter(subscriber => subscriber.context !== context);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {any}
          */
-        emit: function (event) {
+        emit: function emit(event) {
             console.info(event);
+            if (events[event]) {
+                events[event].forEach(subscriber => {
+                    if (subscriber.times === undefined && subscriber.frequency === undefined) {
+                        subscriber.handler.call(subscriber.context);
+                        subscriber.count++;
+                    }
+                    if (subscriber.times) {
+                        subscriber.times--;
+                        subscriber.handler.call(subscriber.context);
+                        subscriber.count++;
+                    }
+                    if (subscriber.frequency && subscriber.count % subscriber.frequency === 0) {
+                        subscriber.handler.call(subscriber.context);
+                        subscriber.count++;
+                    }
+                    // } else {
+                    //     if (subscriber.count % subscriber.frequency === 0) {
+                    //         subscriber.handler.call(subscriber.context)
+                    //         subscriber.count++;
+                    //         subscriber.times--;
+                    //     }
+                    // }
+                });
+            }
+            if (event.includes('.')) {
+                event = event.split('.')[0];
+                emit(event);
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +95,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {any}
          */
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
+            subscribe({ event, context, handler, times });
+
+            return this;
         },
 
         /**
@@ -59,9 +111,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {any}
          */
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
+            subscribe({ event, context, handler, undefined, frequency });
+
+            return this;
         }
     };
 }
